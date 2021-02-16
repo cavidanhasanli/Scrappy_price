846377426b13cb7592c101e23be16af9f15741a4
cavidanhasanli <cavidan.hasanli@mail.ru>
2021-02-16 13:55:58 +0400

    finish code

--- parse_app/tests.py
+++ parse_app/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from django.urls import reverse
+
+
 # Create your tests here.
 
 class TestViews(TestCase):
@@ -7,14 +9,26 @@ class TestViews(TestCase):
         self.client = Client()
 
     def test_home_page(self):
+        """
+        Bu funksiya əsas urlin status kodunu
+        test edir.
+        """
         response = self.client.get('http://localhost:8000/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_link_page(self):
+        """
+        Bu funksiya nümunədə verilmiş urllərə əsasən
+       viewnu test edir.Nümunədə 7 fərqli url mövcuddur.
+
+        """
         data = {
             'url_link': ['https://www.trendyol.com/tonny-black/unisex-spor-ayakkabi-tbqnt-p-44352976',
                          'https://www.suwen.com.tr/p/3-lul-paket-modal-gorunmez-corap-siyah-sc1097361-1884?OM.zn=Category%20-%20Topviews-w24&OM.zpc=SC1097361',
                          'https://www.defacto.com.tr/regular-fit-bisiklet-yaka-sweatshirt-1576268',
-                         'https://www.hepsiburada.com/lufian-joe-smart-gomlek-slim-fit-mavi-p-HBV00000ZHLQL']
+                         'https://www.hepsiburada.com/lufian-joe-smart-gomlek-slim-fit-mavi-p-HBV00000ZHLQL',
+                         'https://www.dsdamat.com/twn-slim-fit-lacivert-armurlu-kumas-ceket-8682060766007/',
+                         'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/kadin/Jean-Ceket/4938489/1426110',
+                         'https://www.a101.com.tr/anne-bebek/mendiva-islak-havlu-gul-60/']
         }
-        self.client.post(reverse('test_link'),data=data)
+        self.client.post(reverse('test_link'), data=data)
--- parse_app/views.py
+++ parse_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 from django.contrib import messages
@@ -6,65 +6,100 @@ from django.contrib import messages
 
 # Create your views here.
 
-def test_views(request):
-    context = {}
+def finish_parser_views(request):
+    """
+    Bu funksiya tərkibində 2 fərqli funksiya işlədir:
+    1) url_check(arg:urls)
+    2)parser_price(arg:urls,arg:soup)
+
+    1 - ci funksiya gələn urlləri yoxlayaraq error mesaj qaytarır.
+    2 - ci funksiya isə gələn url dən istifadə edərək qiyməti
+    istifadəçiyə təqdim edir.
+
+    """
     if request.method == "POST":
 
-        url = request.POST.get("url_link")
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
-        class_list = list()
-        finish_list = set()
-        req = requests.get(url, headers)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        # print(soup)
-        print(req.status_code)
-        if req.status_code == 200:
-            if "suwen" in url:
-                parent = soup.find(class_="product-detail-price-and-container my-4 clearfix")
-                my_str = parent.text
+        urls = request.POST.get("url_link")
 
-                context['data'] = my_str
-                return render(request, 'url.html', context)
+        url = url_check(urls)  # funksiya 1
 
-            else:
-                tags = {tag.name for tag in soup.find_all()}
-
-                for tag in tags:
-                    for i in soup.find_all(tag):
-                        if i.has_attr("class"):
-                            if len(i['class']) != 0:
-                                class_list.append(" ".join(i['class']))
-
-                for c in class_list:
-
-                    if 'prc' in c or 'product__price' in c or 'product_price' in c or 'price' in c:
-                        s = soup.find(class_=c)
-
-                        finish_list.add(s.text)
-
-                context['data'] = list(finish_list)
-                a = context['data']
-                b = []
-
-                for i in range(len(a)):
-
-                    if a[i] != " " and "%" not in a[i] and len(a[i]) < 10 and a[i] != '' and "\n" not in a[i]:
-                        b.append(a[i])
-
-                context['data'] = sorted(b)[0]
-
-                return render(request, 'url.html', context)
+        if url:
+            soup = BeautifulSoup(url.content, 'html.parser')
+            context = parser_price(urls, soup)  # funksiya 2
+            return render(request, 'url.html', context)
 
         else:
-            messages.info(request, 'Bele bir mehsul yoxdur ve ya tehlukesizlik problemi yaranmisdir')
+            messages.info(request, 'Belə bir məhsul yoxdur və ya təhlükəsizlik problemi yaranmışdır')
             return render(request, 'url.html')
     else:
         return render(request, 'url.html')
 
-    return render(request, 'url.html')
+
+def url_check(url):  # funksiya 1
+    """
+    Bu funksiya gələn urlin doğruluğunu
+    yoxlamaq üçün istifadə olunur.
+
+    """
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+    req = requests.get(url, headers)
+    if req.status_code == 200:
+        return req
+    else:
+        return False
+
+
+def parser_price(urls, soup):  # funksiya 2
+    """
+    Bu funksiya gələn doğru urldən
+    istifadə edərək html datalardan
+    qiyməti çıxardıb istifadəçıyə
+    təqdim edir.
+
+    """
+    context = {}
+    class_list = list()
+    finish_set = set()
+
+    if "suwen" in urls:
+        parent = soup.find(class_="product-detail-price-and-container my-4 clearfix")
+        my_str = parent.text
+        context['data'] = my_str
+        return context
+
+    if 'defacto' in urls:
+        parent = soup.find(class_="product-info-prices-new")
+        my_str = parent.text
+        context['data'] = my_str
+        return context
+
+    if 'dsdamat' in urls:
+        parent = soup.find(class_="product__price")
+        my_str = parent.text
+        context['data'] = my_str
+        return context
+
+    else:
+        tags = {tag.name for tag in soup.find_all()}
+
+        for tag in tags:
+            for i in soup.find_all(tag):
+                if i.has_attr("class"):
+                    if len(i['class']) != 0:
+                        class_list.append(" ".join(i['class']))
+
+        for class_ in class_list:
+
+            if 'prc' in class_ or 'product__price' in class_ or 'product_price' in class_ or 'price' in class_:
+                s = soup.find(class_=class_)
+                finish_set.add(s.text)
+
+        finish_list = list(finish_set)
+        context['data'] = sorted(finish_list)[0]
+        return context
